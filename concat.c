ab0876035eac1958f3c041f6b037955501a64530
Leo Famulari <lfamular@gmail.com>
2014-10-10 22:36:46 -0400

    simple concatenator using mempcpy

--- concat.c
+++ concat.c
@@ -6,45 +6,20 @@ void *mempcpy(void *dest, const void *src, size_t n);
 void *combine(void *o1, size_t s1, void *o2, size_t s2);
 
 void *
-combine (void *o1, size_t s1, void *o2, size_t s2)
-{
-	void *result = malloc (s1 + s2);
-	if (result != NULL)
-		mempcpy (mempcpy (result, o1, s1), o2, s2);
-	return result;
-}
-
-void *
 mempcpy(void *dest, const void *src, size_t n)
 {
-	return memcpy(dest, src, n) + n;
+	return (char *)memcpy(dest, src, n) + n;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-	char one[3]  = "ab";
-	char two[3]  = "cd";
-	char three[3]  = "ef";
-	char four[3] = "gh";
-
-	char *alphabet[] = {"ab", "cd", "de", "gh"};
-
 	char *destination = malloc(9);
 	char *ptr = destination;
 
-	ptr = mempcpy(ptr, one, strlen(one)); 
-	puts(destination);
-	ptr = mempcpy(ptr, two, strlen(two));
-	puts(destination);
-/*
-	puts("in for loop");
-	puts("++++");
-	for (int i = 0; i < 4; i++ ) {
-		ptr = mempcpy(ptr,alphabet[i],strlen(alphabet[i]));
-		puts(destination);
+	for (int i = 1; i < argc; i++) {
+		ptr = mempcpy (ptr, argv[i], strlen( argv[i] ));
 	}
-	puts("----");
-*/
 
+	puts(destination);
 	free(destination);	
 }
