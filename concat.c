3b1d35e24ef26c2eef2841e6d223ac03e66024af
Leo Famulari <lfamular@gmail.com>
2014-10-10 22:40:50 -0400

    removed superfluous prototype

--- concat.c
+++ concat.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 
 void *mempcpy(void *dest, const void *src, size_t n);
-void *combine(void *o1, size_t s1, void *o2, size_t s2);
 
 void *
 mempcpy(void *dest, const void *src, size_t n)
