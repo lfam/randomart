3f6e8aa1b7f889328e054d32f8a35159b8fca96b
Leo Famulari <lfamular@gmail.com>
2014-11-08 15:50:34 -0500

    Added comments and error messages

--- randomart.c
+++ randomart.c
@@ -63,6 +63,7 @@ fingerprint_randomart(char *userstr, size_t userstr_len, size_t usr_fldbase)
 	unsigned char	field[fld_x][fld_y];
 	
 	if ((retval = calloc(((size_t)fld_x + 3), ((size_t)fld_y + 2))) == NULL)
+		fprintf(stderr, "ERROR: failed to allocate array.\n");
 		return NULL;
 
 	/* initialize field */
@@ -70,18 +71,21 @@ fingerprint_randomart(char *userstr, size_t userstr_len, size_t usr_fldbase)
 	x = fld_x / 2;
 	y = fld_y / 2;
 
+	/* set up error reporting for strtoul() on user's input */
 	char	*errstring = NULL;
 	if ((errstring = malloc(userstr_len + 1)) == NULL)
+		fprintf(stderr, "ERROR: failed to allocate memory.\n");
 		return NULL;
 	memset(errstring, ' ', userstr_len + 1);
 	errstring[userstr_len] = '\0';
 	char 	*errptr = errstring;
+	/* error flag */
 	int	strtoul_err = 0;
 
-	/* process raw key */
+	/* process user's input */
 	for (i = 0; i < userstr_len; i+=2) {
 
-		/* break off two characters (hex number) */
+		/* break off two characters (i.e. one hex byte) */
 		char	num_str[3];
 		memset(num_str, 0, sizeof(num_str));
 		memcpy(num_str, &userstr[i], sizeof(num_str) - 1);
@@ -112,7 +116,7 @@ fingerprint_randomart(char *userstr, size_t userstr_len, size_t usr_fldbase)
 			strtoul_err = 1 ;
 			continue;
 		} else if (input > UINT_MAX) {
-			fprintf(stderr, "%lu greater than UINT_MAX\n", input);
+			fprintf(stderr, "ERROR: %lu greater than UINT_MAX\n", input);
 			fprintf(stderr, "Not all input will be processed.\n");
 			strtoul_err = 1 ;
 			continue;
@@ -144,7 +148,7 @@ fingerprint_randomart(char *userstr, size_t userstr_len, size_t usr_fldbase)
 
 	if ( strtoul_err != 0 ) {
 		fputs(errstring, stderr);
-		fprintf(stderr, "<-- input characters failed processing\n");
+		fprintf(stderr, "<-- ERROR: not hexadecimal\n");
 	}
 
 	free(errstring);
@@ -178,6 +182,7 @@ fingerprint_randomart(char *userstr, size_t userstr_len, size_t usr_fldbase)
 		*p++ = '-';
 	*p++ = '+';
 
+	/* show input line and return art */
 	puts(userstr);
 	return retval;
 }
@@ -188,12 +193,12 @@ main(int argc, char **argv)
 	char	*line = NULL;
 	size_t	line_buf_len = 0;
 	ssize_t	line_len;
-	ssize_t	usr_fldbase;
 	int	delim = 10; // ASCII for newline
+	ssize_t	usr_fldbase;
 
 	if (argc > 1) {
 		if ((usr_fldbase = strtol(argv[1], NULL, 10)) < 0) {
-			fprintf(stderr, "Field base must be >= 0.\n");
+			fprintf(stderr, "ERROR: field base must be >= 0.\n");
 			return 1; 
 		}
 	} else {
@@ -204,17 +209,17 @@ main(int argc, char **argv)
 	while ((line_len = getdelim(&line, &line_buf_len, delim, stdin)) > 0) {
 		char	*randomart = NULL;
 		if (line == NULL) {
-			fprintf ( stderr,"null pointer dereference of line\n" );
+			fprintf ( stderr,"ERROR: null pointer dereference of line\n" );
 			return 1;
 		} else if (line_len < 0) {
-			fprintf(stderr, "getdelim() returned -1\n");
+			fprintf(stderr, "ERROR: getdelim() returned -1\n");
 		} else {
 			line[line_len - 1] = '\0';
 			randomart = fingerprint_randomart(line, (size_t)line_len - 1, (size_t)usr_fldbase);
 		}
 
 		if (randomart == NULL) {
-			fprintf (stderr,"fingerprint_randomart() returned NULL for input %s\n", line);
+			fprintf (stderr,"ERROR: fingerprint_randomart() returned NULL for input:\n%s\n", line);
 			return 1;
 		} else {
 			memset(line, 0, (size_t)line_len);
